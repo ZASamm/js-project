166e1a23d9b17d6ee87c6476056455bb926b3b20
Zara Samm <zarasamm1996@gmail.com>
2024-10-11 14:47:14 +0000

    add: javascript prompt

---
+++ javascript.js
@@ -0,0 +1,11 @@
+const prompt = require("prompt-sync")()
+
+const name = prompt("What is your name? ")
+
+console.log("hello", name, "welcome to our game")
+
+const shouldWePlay = prompt('Do you want to play? ')
+
+
+
+
